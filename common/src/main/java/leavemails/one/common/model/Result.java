eec552506f5059f6e68d8dac6651f414499a9163
lostsakura <ldx9709@gmail.com>
2022-05-19 23:33:02 +0800

    update

--- common/src/main/java/leavemails/one/common/model/Result.java
+++ common/src/main/java/leavemails/one/common/model/Result.java
@@ -1,11 +1,16 @@
 package leavemails.one.common.model;
 
+import cn.hutool.core.lang.Assert;
+import leavemails.one.common.constants.GlobalStatusCodeConstants;
+import lombok.Data;
+
 import java.io.Serializable;
 
 /**
  * @author lostsakura
  * @date 2022-05-19 16:14
  */
+@Data
 public class Result<T> implements Serializable {
 
     private Integer code;
@@ -13,5 +18,22 @@ public class Result<T> implements Serializable {
     private String msg;
 
     private T data;
+
+    public static <T> Result<T> success(T data) {
+        Result<T> result = new Result<>();
+        result.code = GlobalStatusCodeConstants.SUCCESS.getCode();
+        result.msg = "";
+        result.data = data;
+        return result;
+    }
+
+    public static <T> Result<T> error(Integer code, String message) {
+        Assert.isTrue(!GlobalStatusCodeConstants.SUCCESS.getCode().equals(code), "仅能设置为错误的code");
+        Result<T> result = new Result<>();
+        result.code = code;
+        result.msg = message;
+        return result;
+    }
+
 
 }
