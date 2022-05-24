b902bb5f15c8f47d420334b122725c12d15becc0
lostsakura <ldx9709@gmail.com>
2022-05-24 19:06:21 +0800

    update

---
+++ common/src/main/java/leavemails/one/common/annotation/SysLog.java
@@ -0,0 +1,15 @@
+package leavemails.one.common.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @author lostsakura
+ * @date 2022-05-24 09:17
+ */
+@Documented
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SysLog {
+
+    int age();
+}
---
+++ common/src/main/java/leavemails/one/common/aspect/SysLogAspect.java
@@ -0,0 +1,8 @@
+package leavemails.one.common.aspect;
+
+/**
+ * @author lostsakura
+ * @date 2022-05-24 10:18
+ */
+public class SysLogAspect {
+}
