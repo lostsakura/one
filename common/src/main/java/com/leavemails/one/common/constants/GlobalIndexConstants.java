23e34b45b0152e9d6cb65fc442f11d2edfcdb2b8
lostsakura <ldx9709@gmail.com>
2022-06-08 22:54:01 +0800

    update

--- common/src/main/java/com/leavemails/one/common/constants/GlobalIndexConstants.java
+++ common/src/main/java/com/leavemails/one/common/constants/GlobalIndexConstants.java
@@ -9,6 +9,6 @@ public class GlobalIndexConstants {
     /**
      * 操作日志索引
      */
-    public static final String OPERATION_LOG_INDEX = "operation-log-*";
+    public static final String OPERATION_LOG_INDEX = "operation-log";
 
 }
